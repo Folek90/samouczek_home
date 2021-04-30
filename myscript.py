562b8cf5a5af99df11f603a9f6884e90c7182d34
Folek90 <piotr.f@dianet.pl>
2021-04-30 16:01:54 +0200

    orgranizacja plikow w projekcie, importowanie

---
+++ myscript.py
@@ -0,0 +1,4 @@
+from rockets import RocketBoard
+
+board = RocketBoard(2)
+board2 = RocketBoard()
--- rockets.py
+++ rockets.py
@@ -3,10 +3,10 @@ from random import randint
 
 # metody typu d under - double underscore 
 # __str__ - STRing
-
+'''
 class Rocket:
-    '''mozna dodac opis krotki 
-    '''
+'''#mozna dodac opis krotki 
+'''
 
     def __init__(self, speed=1):
         self.altitude = 0
@@ -15,10 +15,13 @@ class Rocket:
 
 
     def moveUp(self):
-        '''poruszamy sie do gory(altitude) z szybkoscia (speed)
-        '''
+'''#poruszamy sie do gory(altitude) z szybkoscia (speed)
+'''
         self.altitude += self.speed
        
+
+        
+
 
 
 rockets = [Rocket(randint(1,6)) for i in range(5)]
@@ -34,3 +37,70 @@ for rocket in rockets:
 
 
 print(rocket)
+
+'''
+'''
+
+class Rocket:
+
+
+    def __init__(self, speed=1):
+        self.altitude = 0
+
+        self.speed = speed
+
+
+    def moveUp(self):
+
+        self.altitude += self.speed
+       
+    def __str__(self):
+        return ('rakieta jest aktualnie na wysokosci: ' + str(self.altitude))
+        
+
+
+
+rockets = [Rocket(randint(1,6)) for i in range(5)]
+
+
+
+for i in range(10):
+    rocketIndexToMove = randint(0, len(rockets) - 1)
+    rockets[rocketIndexToMove].moveUp()
+
+for rocket in rockets:
+    print(rocket)
+
+'''
+
+from random import randint
+
+class Rocket: 
+    def __init__(self, speed = 1):
+        self.altitude = 0
+        self.speed = speed
+
+    def moveUp(self):
+        self.altitude += self.speed
+
+        def __str__(self):
+            return "Rakieta aktualnie jest na wysokosci: ", str(self.altitude)
+
+
+class RocketBoard:
+    def __init__(self, amountOfRockets=2):
+        rockets = [Rocket(randint(1,6)) for i in range(5)]
+
+        for i in range(10):
+            rocketIndexToMove = randint(0, len(rockets) - 1)
+            rockets[rocketIndexToMove].moveUp()
+
+        for rocket in rockets:
+            print(rocket)
+
+
+ 
+    
+
+        
+
