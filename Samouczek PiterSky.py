ff007557b00079ac6f14442b52311b0ca6efe12b
Folek90 <piotr.f@dianet.pl>
2021-05-14 16:19:20 +0200

    aktualizacja

--- Samouczek PiterSky.py
+++ Samouczek PiterSky.py
@@ -1407,6 +1407,8 @@ print(a_square.area())
 print()
 
 
+
+
 class Dog:
     def __init__(self, name, breed, owner):
         self.name = name
@@ -1693,6 +1695,96 @@ mark = bobby
 
 print(bobby is not mark)
 print() 
+
+
+
+# ______________________________ ŁĄCZENIE WSZYSTKIEGO W CAŁOSC [ GRA W KARTY ] __________________________________
+
+
+
+class Card:
+    suits = ['pik', 'kier', 'karo', 'trefl']
+    values = [None, None, '2', '3', '4', '5', '6', '7', '8', '9', '10', 'walet', 'dama', 'król', 'as']
+
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+
+    def __lt__(self, other):
+        if self.value < other.value:
+            return True
+        if self.value == other.value:
+            if self.suit < other.suit:
+                return True
+            else:
+                return False
+        return False
+
+
+    def __gt__(self, other):
+        if self.value > other.value:
+            return True
+        if self.value == other.value:
+            if self.suit > other.suit:
+                return True
+            else:
+                return False
+        return False
+
+
+    def __repr__(self):
+        return self.values[self.value] + " " + self.suits[self.suit]
+
+card1 = Card(10, 2)
+card2 = Card(11, 3)
+
+print(card1 < card2)
+
+card3 = Card(4, 1)
+
+print(card3)
+print()
+
+
+# ___________________________________ TWORZENIE KLASY REPREZENTUJACEJ  TALIE KART ____________________________________
+
+
+from random import shuffle
+
+
+class Deck:
+    def __init__(self):
+        self.cards = [] 
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards.append(Card(i, j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
+
+
+deck = Deck()
+for card in deck.cards:
+    print(card)
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
 
 
 
